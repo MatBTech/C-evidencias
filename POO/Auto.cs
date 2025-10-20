b53cfdbc38a4d2dd3223be92633e505a225488d3
199831-web <danielr19822009@gmail.com>
2025-10-20 15:00:06 -0500

    Agregar archivos de proyecto.

---
+++ POO/Auto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAuto
+{   
+    internal class Auto
+    {
+        public string Marca { get; set; }
+        public string Modelo {  get; set; }
+        public int Año { get; set; }
+        public Auto(string marca, string modelo, int año) {
+            Marca = marca;
+            Modelo = modelo;
+            Año = año;
+        }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Marca: {Marca}\nModelo: {Modelo} \nAño: {Año}");
+        }
+    }
+    
+}
---
+++ POO/Estudiante.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es
+{
+    internal class Estudiante
+    {
+        
+
+        public string Nombre {  get; set; }
+        public int Edad {  get; set; }
+        public string Direccion {  get; set; }
+
+        public Estudiante(string nombre, int edad, string direccion)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Direccion = direccion;
+        }
+
+        public void VerificarEdad()
+        {
+            if (Edad >= 18)
+            {
+                Console.WriteLine($"{Nombre}Eres mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine($"{Nombre}Eres menor de edad");
+            }
+        }
+    }
+}
